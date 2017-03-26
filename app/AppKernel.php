4f0ca75c2ca2699d49ff165ac83a80ecda956e2e
puterakahfi <puterakahfi@gmail.com>
2017-03-26 11:46:49 +0700

    config routing for library

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Presentation\Library\RestBundle\PresentationLibraryRestBundle(),
+            new Presentation\Library\AppBundle\PresentationLibraryAppBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
